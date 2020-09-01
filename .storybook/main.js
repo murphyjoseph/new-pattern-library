9df32689c038f254bf44f26a2cdc64eaf925ce58
Joseph Murphy <murphydjoseph@gmail.com>
2020-09-01 15:50:01 -0500

    got storybook working

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,10 @@
 // }
 
 module.exports = {
-  stories: ['../src/components/**/*.stories.@(tsx|mdx)'],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-links",
