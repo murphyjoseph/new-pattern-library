5f17c95d9dcfe1e9eb6b3fc31351318315474736
Joseph Murphy <murphydjoseph@gmail.com>
2020-09-21 18:45:59 -0500

    flag and positioner added

---
+++ src/foundation/positioner/positioner.interface.ts
@@ -0,0 +1,4 @@
+import { IPattern } from '../../shared/interfaces/pattern.interface';
+import { IStylesDisplay, IStylesPosition } from '../../shared/interfaces/styles.interface';
+
+export interface IPositioner extends IPattern, IStylesDisplay, IStylesPosition {}
